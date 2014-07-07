95a55fa3eed6b7443b9f032a3fd673638b727f55
Sebazbeer <sebastianperez@MacBook-Pro-de-sebastian.local>
2014-07-07 01:07:08 -0400

    sdsd

--- frmregistro.php
+++ frmregistro.php
@@ -1,8 +1,5 @@
 <?php
 
-include_once("conexion.php");
-
-
 
 $tipo_camara = $_POST['tipo_camara'];
 $marca = $_POST['marca'];
@@ -18,19 +15,29 @@ $tienda2 = $_POST['tienda2'];
 $tienda3 = $_POST['tienda3'];
 $tienda4 = $_POST['tienda4'];
 $caracteristicas = $_POST['caracteristicas'];
-//$imagen = $_POST['imagen'];
-//$imagen1 = $_POST['imagen1'];
-//$imagen2 = $_POST['imagen2'];
-//$imagen3 = $_POST['iamgen3'];
-//$imagen4 = $_POST['imagen4'];
+
+
+include_once("conexion.php");
 
 
 $dbQuery=mysql_query("INSERT INTO productos (tipo_camara, marca, modelo, valor, valor1, valor2, valor3, valor4, tienda, tienda1, tienda2, tienda3, tienda4, caracteristicas, imagen, imagen1, imagen2, imagen3, imagen4, ) VALUES ('".$tipo_camara."','".$marca."','".$modelo."','".$valor."','".$valor1."','".$valor2."','".$valor3."','".$valor4."','".$tienda."','".$tienda1."','".$tienda2."','".$tienda3."','".$tienda4."','".$caracteristicas."');");
 
-echo"<script type=\"text/javascript\">alert('Producto Agregado Correctamente'); window.location='agregar.php';</script>";
+if ($dbQuery = true)){ 
 
+echo"<script type=\"text/javascript\">
+alert('Producto Agregado Correctamente');
+ window.location='agregar.php';
+ </script>";
+}else{
 
+"<script type=\"text/javascript\">
+alert('Error');
+ window.location='agregar.php';
+ </script>";
+
+}
 mysql_close($conexion);
+		
 
 
 
