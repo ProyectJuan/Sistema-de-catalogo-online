e9aa99df286c85f133a626cfbefd09ac0cbb7327
Sebazbeer <sebastianperez@MacBook-Pro-de-sebastian.local>
2014-07-11 01:28:18 -0400

    probando

--- frmregistro.php
+++ frmregistro.php
@@ -17,12 +17,31 @@ $tienda3 = $_POST['tienda3'];
 $tienda4 = $_POST['tienda4'];
 $tienda5 = $_POST['tienda5'];
 $imagen = $_POST['imagen'];
+$tipo_imagen= $_POST('tipo_imagen')
+
+if ( ! isset($_FILES["imagen"]) || $_FILES["imagen"]["error"] > 0){
+    echo "ha ocurrido un error";
+} else {
+    //ahora vamos a verificar si el tipo de archivo es un tipo de imagen permitido.
+    //y que el tamano del archivo no exceda los 16mb
+    $permitidos = array("image/jpg", "image/jpeg", "image/gif", "image/png");
+    $limite_kb = 16384; //16mb es el limite de medium blob
+     
+    if (in_array($_FILES['imagen']['type'], $permitidos) && $_FILES['imagen']['size'] <= $limite_kb * 1024){
+     
+        //este es el archivo temporal
+        $imagen_temporal  = $_FILES['imagen']['tmp_name'];  
+        //este es el tipo de archivo
+        $tipo = $_FILES['imagen']['type'];
+        //leer el archivo temporal en binario
+        $fp     = fopen($imagen_temporal, 'r+b');
+        $data = fread($fp, filesize($imagen_temporal));
+        fclose($fp);
+        //escapar los caracteres
+        $data = mysql_escape_string($data);
 
 
-
-
-
-$dbQuery=mysql_query("INSERT INTO camaras VALUES('','$tipo_camara','$marca','$modelo','$precio1','$precio2','$precio3','$precio4','$precio5','$caracteristicas','$tienda1','$tienda2','$tienda3','$tienda4','$tienda5','$imagen')");
+$dbQuery=mysql_query("INSERT INTO camaras VALUES('','$tipo_camara','$marca','$modelo','$precio1','$precio2','$precio3','$precio4','$precio5','$caracteristicas','$tienda1','$tienda2','$tienda3','$tienda4','$tienda5','$imagen','$tipo_imagen')");
          
 
 if ($dbQuery = true){ 
@@ -39,4 +58,9 @@ alert('Error');
  </script>";
 
 }
+
+else {
+        echo "archivo no permitido, es tipo de archivo prohibido o excede el tamano de $limite_kb Kilobytes";
+    }
+  }
 ?>
