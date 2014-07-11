e977dce391be86c400aad0e02287a8810cd80522
Sebazbeer <sebastianperez@MacBook-Pro-de-sebastian.local>
2014-07-11 01:37:13 -0400

    qq

--- frmregistro.php
+++ frmregistro.php
@@ -16,7 +16,7 @@ $tienda2 = $_POST['tienda2'];
 $tienda3 = $_POST['tienda3'];
 $tienda4 = $_POST['tienda4'];
 $tienda5 = $_POST['tienda5'];
-$imagen = $_POST['imagen'];
+
 
 if ( ! isset($_FILES["imagen"]) || $_FILES["imagen"]["error"] > 0){
     echo "ha ocurrido un error";
@@ -40,7 +40,7 @@ if ( ! isset($_FILES["imagen"]) || $_FILES["imagen"]["error"] > 0){
         $data = mysql_escape_string($data);
 
 
-$dbQuery=mysql_query("INSERT INTO camaras VALUES('','$tipo_camara','$marca','$modelo','$precio1','$precio2','$precio3','$precio4','$precio5','$caracteristicas','$tienda1','$tienda2','$tienda3','$tienda4','$tienda5','$data','$tipo')");
+$dbQuery=mysql_query("INSERT INTO camaras (imagen, tipo_imagen) VALUES('','$tipo_camara','$marca','$modelo','$precio1','$precio2','$precio3','$precio4','$precio5','$caracteristicas','$tienda1','$tienda2','$tienda3','$tienda4','$tienda5','$data','$tipo')");
          
 
 if ($dbQuery = true){ 
