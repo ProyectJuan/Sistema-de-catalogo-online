d66b8f00d10e8338c1bcd7b22a7c2444f9deedbb
lalina <ailin.silva@gmail.com>
2014-07-12 02:02:51 -0400

    menu.php

---
+++ menu.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Menú | Administrador</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+	<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="contactinfo">
+							<ul class="nav nav-pills">
+								<li><a href=""><i class="fa fa-phone"></i>+56 9 5000000</a></li>
+								<li><a href=""><i class="fa fa-envelope"></i> lamejoropcion@gmail.cl</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href=""><i class="fa fa-facebook"></i></a></li>
+								<li><a href=""><i class="fa fa-twitter"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header_top-->
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="index.html"><img src="images/home/lamejor.jpg" alt="" /></a>
+						</div>
+				
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								
+								<li><a href="login.html" class="active"><i class="fa fa-lock"></i>Iniciar Sesión</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.html">Inicio</a></li>
+								<li class="dropdown"><a href="#">Productos<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="shop.html">Camaras</a></li> <!--
+								
+								<li><a href="contact-us.html">Contacto</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="search_box pull-right">
+							<input type="text" placeholder="BUSCAR"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
+
+	</header><!--/header-->
+	<div class="login-form">
+	<div class="container">
+        <h3>Administrador</h3>
+        <p> Como administrador tiene los privilegios maximos dentro de este sistema, por lo que se recomienda guardar de forma inmediata cualquier tipo de modificacion realizada.</p>
+        </div>
+	<!--<section id="form">--><!--form-->
+		<div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h3>Ver Inventario</h3>
+          <p>Acceso total a toda la informacion de Inventario, puede realizar busqueda de productos especificos.</p>
+          <p><a class="btn btn-default" href="Vista1.php" role="button">Ver &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h3>Agregar usuario</h3>
+          <p>Para agregar un nuevo usuario al sistema, debe completar este formulario </p>
+          <p><a class="btn btn-default" href="Registro.php" role="button">Continuar &raquo;</a></p>
+       </div>
+        <div class="col-md-4">
+          <h3>Editar Informacion Inventario</h3>
+          <p>Al momento de actualizar, agregar informacion, asegure guardar todos los cambios realizados</p>
+          <p><a class="btn btn-default" href="modificar.php" role="button">Continuar &raquo;</a></p>
+        </div>
+      </div>
+      <div class="row">
+      <div class="col-md-4">
+          <h3>Agregar nuevo Producto</h3>
+          <p>Para agregar un nuevo producto al sistema de inventario debe completar el sigueinte formulario con la informacion requerida</p>
+          <p><a class="btn btn-default" href="Formulario.php" role="button">Continuar &raquo;</a></p>
+        </div>
+      <div class="col-md-4">
+          <h3>Generar Reportes</h3>
+          <p></p>
+          <p><a class="btn btn-default" href="Reportes.php" role="button">Continuar &raquo;</a></p>
+        </div>
+         <div class="col-md-4">
+          <h3>Mensajes</h3>
+          <p></p>
+          <p><a class="btn btn-default" href="verMensaje.php" role="button">Ver &raquo;</a></p>
+        </div>
+      </div>
+      <div>
+              <div class="col-md-4">
+          <h3>Ver Reportes</h3>
+          <p></p>
+          <p><a class="btn btn-default" href="Vreportes.php" role="button">Ver &raquo;</a></p>
+        </div>
+          <div class="col-md-4">
+          <h3>Editar Perfir de Usuarios</h3>
+          <p></p>
+          <p><a class="btn btn-default" href="modificaruser.php" role="button">Ver &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h3>Ver Perfiles de Usuarios</h3>
+          <p></p>
+          <p><a class="btn btn-default" href="VistaPerfil.php" role="button">Ver &raquo;</a></p>
+        </div>
+      </div>  
+      <div>
+      </div>
+      
+        
+					</div><!--/formulario agregar producto-->
+				</div>
+				
+					
+					</div><!--/sign up form-->
+				</div>
+			</div>
+		</div>
+	
+	
+	
+	<footer id="footer"><!--Footer-->
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="companyinfo">
+							<h2><span>L</span>a Mejor Opcion</h2>
+							<p>Busca tus camaras preferidas, en todas las tiendas donde se encuentran disponibles.</p>
+						</div>
+					</div>
+					<div class="col-sm-7">
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe1.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Circle of Hands</p>
+								<h2>24 DEC 2014</h2>
+							</div>
+						</div>
+						
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe2.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Circle of Hands</p>
+								<h2>24 DEC 2014</h2>
+							</div>
+						</div>
+						
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe3.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Circle of Hands</p>
+								<h2>24 DEC 2014</h2>
+							</div>
+						</div>
+						
+						<div class="col-sm-3">
+							<div class="video-gallery text-center">
+								<a href="#">
+									<div class="iframe-img">
+										<img src="images/home/iframe4.png" alt="" />
+									</div>
+									<div class="overlay-icon">
+										<i class="fa fa-play-circle-o"></i>
+									</div>
+								</a>
+								<p>Circle of Hands</p>
+								<h2>24 DEC 2014</h2>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="address">
+							<img src="images/home/mapa.jpg" alt=""<p>Region de la Araucania, Temuco, Chile</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2014 ProyectJuan.</p>
+					<p class="pull-right">Designed by <span><a target="_blank" href="">ProyectJuan</a></span></p>
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+	
+
+  
+    <script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.scrollUp.min.js"></script>
+	<script src="js/price-range.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
