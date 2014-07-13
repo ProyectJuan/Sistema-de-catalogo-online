134e26dfb03f07aa7bf31c2d1faf67791b8c297e
lalina <ailin.silva@gmail.com>
2014-07-12 23:54:53 -0400

    ksdjf

--- ingreso_admin.php
+++ ingreso_admin.php
@@ -6,7 +6,6 @@ include_once("conexion.php");
 		$usuario = $_POST['usuario'];
 		$apellido = $_POST['nombre_com'];
 		$password = $_POST['password'];
-		$password1 = $_POST['password1'];
 		$descripcion = $_POST['descripcion'];
 		$tipouser = $_POST['tipouser'];
 		$email = $_POST['email'];
@@ -16,9 +15,9 @@ include_once("conexion.php");
 
 		if ($_POST['password']== $_POST['password1']) {
 	
-				$dbQuery=mysql_query("INSERT INTO usuarios (usuario, nombre_com, password, descripcion, tipouser, email ) VALUES ('".$usuario."','".$nombre_com."','".$password."','".$descripcion."','".$tipouser."','".$email."');");
+				$dbQuery=mysql_query("INSERT INTO usuarios (usuario, nombre_com, password, descripcion, email ) VALUES ('".$usuario."','".$nombre_com."','".$password."','".$descripcion."','".$email."');");
    	             
-				 echo"<script type=\"text/javascript\">alert('Usuario Agregado Exitosamente'); window.location='menu.php';</script>";
+				 echo"<script type=\"text/javascript\">alert('Usuario Agregado Exitosamente'); window.location='login.html';</script>";
 				 
 
 					}else{
