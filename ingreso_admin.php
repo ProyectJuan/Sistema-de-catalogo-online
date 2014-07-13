b24d85f3025bd1d441bb3ce7a4c8b0f842043030
lalina <ailin.silva@gmail.com>
2014-07-12 23:51:23 -0400

    jkh

--- ingreso_admin.php
+++ ingreso_admin.php
@@ -8,6 +8,7 @@ include_once("conexion.php");
 		$password = $_POST['password'];
 		$password1 = $_POST['password1'];
 		$descripcion = $_POST['descripcion'];
+		$tipouser = $_POST['tipouser'];
 		$email = $_POST['email'];
 
 
@@ -15,7 +16,7 @@ include_once("conexion.php");
 
 		if ($_POST['password']== $_POST['password1']) {
 	
-				$dbQuery=mysql_query("INSERT INTO usuarios (usuario, nombre_com, password, descripcion, email ) VALUES ('".$usuario."','".$nombre_com."','".$password."','".$descripcion."','".$email."');");
+				$dbQuery=mysql_query("INSERT INTO usuarios (usuario, nombre_com, password, descripcion, tipouser, email ) VALUES ('".$usuario."','".$nombre_com."','".$password."','".$descripcion."','".$tipouser."','".$email."');");
    	             
 				 echo"<script type=\"text/javascript\">alert('Usuario Agregado Exitosamente'); window.location='menu.php';</script>";
 				 
